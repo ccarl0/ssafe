ae6b9582f64166c1db8ca66bceda8cc8375dae03
ccarl0 <carlo.cecco0@gmail.com>
2024-02-16 15:45:54 +0100

    OAuth 2.0

---
+++ ssafe/ssafe-GA/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using ssafe_GA.Models;
+using System.Diagnostics;
+
+namespace ssafe_GA.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ssafe/ssafe-GA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ssafe_GA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
